516b6267d1e7420da9fda1ee864cbfed619cee86
David Walton <davidwalton6772@gmail.com>
2020-07-31 16:00:29 -0500

    initial npm install and router set up

---
+++ server/modules/routes/movie.router.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const pool = require('../pool');
+
+const movieRouter = express.Router();
+
+movieRouter.get('/', (req, res) => {
+  const queryText = 'SELECT * FROM genres';
+  pool
+    .query(queryText)
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((err) => {
+      console.log('Error completing GET movies list', err);
+      res.sendStatus(500);
+    });
+});
+
+movieRouter.get('/details/:id', (req, res) => {
+  const queryText = 'SELECT * FROM movies WHERE id=$1';
+  pool
+    .query(queryText, [req.params.id])
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((err) => {
+      console.log('Error completing GET details', err);
+      res.sendStatus(500);
+    });
+});
+
+movieRouter.post('/', (req, res) => {
+  const newMovie = req.body;
+  const queryText = `INSERT INTO movies ("title", "description")
+                    VALUES ($1, $2)`;
+  const queryValues = [newMovie.title, newMovie.description];
+  pool
+    .query(queryText, queryValues)
+    .then(() => {
+      res.sendStatus(201);
+    })
+    .catch((err) => {
+      console.log('Error completing POST newMovie', err);
+      res.sendStatus(500);
+    });
+});
+
+movieRouter.put('/', (req, res) => {
+  const updatedMovie = req.body;
+
+  const queryText = `UPDATE movies
+  SET "title" = $1, 
+  "description" = $2, 
+  WHERE id=$3;`;
+
+  const queryValues = [
+    updatedMovie.title,
+    updatedMovie.description,
+    updatedMovie.id,
+  ];
+
+  pool
+    .query(queryText, queryValues)
+    .then(() => {
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      console.log('Error completing PUT movies', err);
+      res.sendStatus(500);
+    });
+});
+
+movieRouter.delete('/:id', (req, res) => {
+  const queryText = 'DELETE FROM movies WHERE id=$1';
+  pool
+    .query(queryText, [req.params.id])
+    .then(() => {
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      console.log('Error completing DELETE movie', err);
+      res.sendStatus(500);
+    });
+});
+
+module.exports = movieRouter;
--- server/server.js
+++ server/server.js
@@ -2,15 +2,17 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
+const movieRouter = require('./modules/routes/movie.router');
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
 app.use(express.static('build'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 /** ---------- ROUTES ---------- **/
-
+app.use('/api/movie', movieRouter);
 
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {
-    console.log('Listening on port: ', port);
+  console.log('Listening on port: ', port);
 });
